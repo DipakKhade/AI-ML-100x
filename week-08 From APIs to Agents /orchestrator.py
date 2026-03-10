6dca721f5358c5ae70779d222d0af8687e6849c5
Dipak Khade <dipakhade214@gmail.com>
2026-03-10 11:48:55 +0530

    added systme prompt

--- week-08 From APIs to Agents /orchestrator.py
+++ week-08 From APIs to Agents /orchestrator.py
@@ -10,11 +10,27 @@ agent = OpenAI(
     api_key= os.environ.get('OPEN_ROUTER_API_KEY')
 )
 
+system_prompt = '''
+    Hey you are a orchestrator and you orchestrator between the different agents 
+        for now you have 2 againt
+        Agent list:
+            1. Researcher Agent
+            2. Coding Agent
+        
+    you have to respond back me with the follwing type of json on the basis of the task asked by the user
+
+    {
+        agent_to_use: researcher_agent | coding_agent
+    }
+
+'''
+
 res = agent.chat.completions.create(
     model='openrouter/free',
     messages= [
-        {'role': 'user', 'content': 'hello sir'},
+        {'role': 'developer', 'content': system_prompt},
+        {'role': 'user', 'content': 'write a rust fn to add vec of numbers'}
     ]
 )
 
-print(res)
+print(res.choices[0].message.content)
