31e1d52151743a9541c464d3cd8a86ad706bb625
Dipak Khade <dipakhade214@gmail.com>
2026-03-15 13:38:54 +0530

    added docs loader

--- week-09 RAG from the Ground Up - Part 1 /rag_01.py
+++ week-09 RAG from the Ground Up - Part 1 /rag_01.py
@@ -6,3 +6,9 @@
     4. store that embedding to vector db e.g chromadb, qudrant, etc
     5. get the relevent embedding based on user prompt and attach it with the user prompt
 """
+
+from langchain_community.document_loaders import PyPDFLoader
+
+data = PyPDFLoader(file_path='/Users/dipakkhade/projects/AI-ML-100x/week-09 RAG from the Ground Up - Part 1 /docs/Mahabharata.pdf')
+docs = data.load()
+print(docs[:10])
