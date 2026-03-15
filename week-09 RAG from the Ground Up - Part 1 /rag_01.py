fe9ee8ae06cdcec5830d17c8936f556210fc37bc
Dipak Khade <dipakhade214@gmail.com>
2026-03-15 13:45:44 +0530

    making chunks

--- week-09 RAG from the Ground Up - Part 1 /rag_01.py
+++ week-09 RAG from the Ground Up - Part 1 /rag_01.py
@@ -8,7 +8,17 @@
 """
 
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 data = PyPDFLoader(file_path='/Users/dipakkhade/projects/AI-ML-100x/week-09 RAG from the Ground Up - Part 1 /docs/Mahabharata.pdf')
 docs = data.load()
 print(docs[:10])
+
+
+txt_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=400 
+)
+
+chunks = txt_splitter.split_documents(documents=docs)
+
