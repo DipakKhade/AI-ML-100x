4cb253c77d4a14ddadbe5f759c1043fe21580fd1
Dipak Khade <dipakhade214@gmail.com>
2026-03-15 13:27:07 +0530

    steps init

---
+++ week-09 RAG from the Ground Up - Part 1 /rag_01.py
@@ -0,0 +1,8 @@
+
+"""
+    1. read the data
+    2. create a chunks out of it
+    3. make embedding from chunks
+    4. store that embedding to vector db e.g chromadb, qudrant, etc
+    5. get the relevent embedding based on user prompt and attach it with the user prompt
+"""
